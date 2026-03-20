4d7f192ffefc81941cff55c46a43a0224c435a4e
Edwin <emunoz9@neiu.edu>
2026-03-20 12:53:07 -0500

    Fix local public asset paths

--- frontend/src/utils/resolveAssetHref.js
+++ frontend/src/utils/resolveAssetHref.js
@@ -1,5 +1,10 @@
+import { normalizePath } from './normalizePath.js'
+
 export function resolveAssetHref(path) {
   const normalizedPath = path.replace(/^\/+/, '')
+  const currentPath = typeof window === 'undefined' ? '/' : normalizePath(window.location.pathname)
+  const pathDepth = currentPath === '/' ? 0 : currentPath.replace(/^\/+|\/+$/g, '').split('/').length
+  const relativePrefix = pathDepth > 0 ? '../'.repeat(pathDepth) : './'
 
-  return new URL(`../${normalizedPath}`, import.meta.url).href
+  return `${relativePrefix}${normalizedPath}`
 }
